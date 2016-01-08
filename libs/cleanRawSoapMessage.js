46b9d6b5cf7a7a06405d6feb702e50f0426fb2d9
Ben Howl <benjamin.howl@anz.com>
2016-01-08 18:09:16 +1100

    reformatted

--- libs/cleanRawSoapMessage.js
+++ libs/cleanRawSoapMessage.js
@@ -1,30 +1,30 @@
- var cleanRawSoapMessage = function cleanRawSoapMessage(rawMessage){
-   var scanMessageFromPosition, cleansedMessage;
-   scanMessageFromPosition = 0;
+var cleanRawSoapMessage = function cleanRawSoapMessage(rawMessage) {
+  var scanMessageFromPosition, cleansedMessage;
+  scanMessageFromPosition = 0;
 
-   if (rawMessage.toLowerCase().indexOf('<soap') != -1) {
-     scanMessageFromPosition = rawMessage.toLowerCase().indexOf('<soap');
-   }
+  if (rawMessage.toLowerCase().indexOf('<soap') != -1) {
+    scanMessageFromPosition = rawMessage.toLowerCase().indexOf('<soap');
+  }
 
-   cleansedMessage =
-     rawMessage.substr(scanMessageFromPosition)
-       .replace(/(soap-env|soapenv)/gi, 'SOAP-ENV')
-       .replace(/<([a-z0-9\-]+):/gi, function(str) {
-         if (str != '<SOAP-ENV:') {
-           return '<';
-         } else {
-           return str;
-         }
-       })
-       .replace(/<\/([a-z0-9\-]+):/gi, function(str) {
-         if (str != '</SOAP-ENV:') {
-           return '</';
-         } else {
-           return str;
-         }
-       });
+  cleansedMessage =
+    rawMessage.substr(scanMessageFromPosition)
+      .replace(/(soap-env|soapenv)/gi, 'SOAP-ENV')
+      .replace(/<([a-z0-9\-]+):/gi, function (str) {
+        if (str != '<SOAP-ENV:') {
+          return '<';
+        } else {
+          return str;
+        }
+      })
+      .replace(/<\/([a-z0-9\-]+):/gi, function (str) {
+        if (str != '</SOAP-ENV:') {
+          return '</';
+        } else {
+          return str;
+        }
+      });
 
-   return cleansedMessage;
- };
+  return cleansedMessage;
+};
 
 module.exports = cleanRawSoapMessage;
